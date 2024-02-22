b88a83292e716cc28db5925cf195f0c8c62381af
depchai1511 <natibatian@gmail.com>
2024-02-22 18:00:47 +0300

    fix : fix main.js file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "@/App.vue";
+import router from "@/router";
+import { persist } from 'pinia-persists'
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+pinia.use(persist())
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+const app = createApp(App);
+
+app.use(pinia)
+app.use(router);
+app.mount("#app");
