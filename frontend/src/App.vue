122dfad65a54483e54bd058156d1be197d194845
depchai1511 <natibatian@gmail.com>
2024-02-22 18:00:14 +0300

    fix : fix app file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavBar></NavBar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "../src/components/NavBar.vue"
 
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
-  }
+    NavBar,
+  },
+ 
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import '@/assets/main.css';
+@import '@/assets/light.css';
+@import '@/assets/dark.css';
 </style>
