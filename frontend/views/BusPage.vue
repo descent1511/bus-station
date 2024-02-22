92e71f61955cd1786eab6f5eebbbe537dfd3a3ce
depchai1511 <natibatian@gmail.com>
2024-02-22 17:51:36 +0300

    feat : create Bus page

---
+++ frontend/views/BusPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <BusList></BusList>
+</template>
+  
+<script>
+
+import BusList from "@/components/BusList.vue"
+
+export default {
+    name: 'BusPage',
+    components: {
+        BusList,
+
+    },
+}
+</script>
+
+<style scoped></style>
