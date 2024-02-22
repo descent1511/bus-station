96d9cf2f2ba2fe379824f0f6be8d8fc6c91d367c
depchai1511 <natibatian@gmail.com>
2024-02-22 17:51:54 +0300

    feat : create home page

---
+++ frontend/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <FindRouteForm></FindRouteForm>
+    <ServiceInf></ServiceInf>
+
+</template>
+  
+<script>
+
+import FindRouteForm from "@/components/FindRouteForm.vue"
+import ServiceInf from "@/components/ServiceInf.vue"
+export default {
+name: 'HomePage',
+components: {
+    FindRouteForm,
+    ServiceInf
+},
+}
+</script>
+  
+<style scoped>
+
+</style>
