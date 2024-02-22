45f8759dbd35d18d8ec0de4c067d42814e399507
depchai1511 <natibatian@gmail.com>
2024-02-22 18:07:52 +0300

    fix : fix vue congif file

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader') 
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+};
