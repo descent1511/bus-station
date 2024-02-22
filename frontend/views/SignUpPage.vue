296a0d554d0a8b6e97d4aba12f92bb39858031a1
depchai1511 <natibatian@gmail.com>
2024-02-22 17:53:32 +0300

    feat : create signUp  page

---
+++ frontend/views/SignUpPage.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <SignUpForm></SignUpForm>
+
+
+</template>
+  
+<script>
+
+import SignUpForm from "../components/SignUpForm.vue"
+export default {
+name: 'SignUpPage',
+components: {
+
+    SignUpForm
+},
+}
+</script>
+  
+<style scoped>
+
+
+</style>
