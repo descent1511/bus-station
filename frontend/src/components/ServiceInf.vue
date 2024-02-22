91b2a97837f5be9f0e6fab54c70cce72805d3816
depchai1511 <natibatian@gmail.com>
2024-02-22 17:43:44 +0300

    feat : create component for seviceInf

---
+++ frontend/src/components/ServiceInf.vue
@@ -0,0 +1,130 @@
+<template>
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR SERVICES</h2>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                    <img src="../assets/driver.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Driver</h5>
+                        
+                        <div class="mb-4">
+                            <h6 class="mb-1">Skills</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Safety
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Patience
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Alertness
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Communication
+                            </span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h6 class="mb-1">Experience</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                > 10 years
+                            </span>
+
+                        </div>
+
+                        <div class="d-flex justify-content-evenly mb-2">
+                            <a href="/driver" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                    <img src="../assets/buses.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Bus</h5>
+
+                     
+
+                        <div class="mb-4">
+                            <h6 class="mb-1">Capacity</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                30
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                40
+                            </span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h6 class="mb-1">Facilities</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Wifi
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Television
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Air conditioning
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Bed
+                            </span>
+
+                        </div>
+                        <div>
+                            <div class="d-flex justify-content-evenly mb-2">
+                                <a href="/bus" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                    <img src="../assets/route.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Route</h5>
+
+                        <div class="mb-4">
+                            <h6 class="mb-1">Location</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Airport
+                            </span>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                Supermarket
+                            </span>
+                        </div>
+
+                        <div class="mb-4">
+                            <h6 class="mb-1">Total number of completed routes</h6>
+                            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                10
+                            </span>
+                         
+
+                        </div>
+                        <div>
+                            <div class="d-flex justify-content-evenly mb-2">
+                                <a href="/bus" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+  
+<style scoped></style>
