d7090ec1a7a478e1bdff78c64dd9d64aaf468c3c
depchai1511 <natibatian@gmail.com>
2024-02-22 17:53:09 +0300

    feat : create schedule page

---
+++ frontend/views/SchedulePage.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <ScheduleList></ScheduleList>
+
+</template>
+  
+<script>
+
+import ScheduleList from "@/components/ScheduleList.vue"
+
+export default {
+name: 'DriverPage',
+components: {
+    ScheduleList,
+   
+},
+}
+</script>
+  
+<style scoped>
+
+</style>
