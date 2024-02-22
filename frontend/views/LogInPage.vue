5fd85f6328b1e529dff3af8345782883a2386aaa
depchai1511 <natibatian@gmail.com>
2024-02-22 17:52:20 +0300

    feat : create login page

---
+++ frontend/views/LogInPage.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <LogInForm></LogInForm>
+
+</template>
+  
+<script>
+
+import LogInForm from "@/components/LogInForm.vue"
+export default {
+name: 'LogInPage',
+components: {
+
+    LogInForm
+},
+}
+</script>
+  
+<style scoped>
+
+</style>
