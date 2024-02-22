d68b0ecc328a0d20eff737d43255e10743257088
depchai1511 <natibatian@gmail.com>
2024-02-22 17:52:08 +0300

    feat : create driver page

---
+++ frontend/views/DriverPage.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <DriverList></DriverList>
+
+</template>
+  
+<script>
+
+import DriverList from "@/components/DriverList.vue"
+
+export default {
+name: 'DriverPage',
+components: {
+    DriverList,
+   
+},
+}
+</script>
+  
+<style scoped>
+
+</style>
